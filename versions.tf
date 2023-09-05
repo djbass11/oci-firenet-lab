888c90725d6b1fd77170cf540c87594b2fa35d15
djbass11 <117679484+djbass11@users.noreply.github.com>
2023-09-04 19:41:11 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
     aviatrix = {
-      source  = "AviatrixSystems/aviatrix"
-      version = "~> 3.0.1"
- required_version = ">= 1.0"
+      source = "AviatrixSystems/aviatrix"
+      version = "3.1.2"
+    }
+  }
+}
+
+provider "aviatrix" {
+  # Configuration options
 }
